869ccb83406f97a97408fbbec9487ed50f23602d
manisha-mv <manishamanivannan1505@gmail.com>
2026-01-31 13:32:50 +0530

    problem2.c

--- While_loop/problem2.c
+++ While_loop/problem2.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
 
 int main(){
-    int n;
-    int balance;
-    int transfer;
-    int count =0;
-    int i ;
-
+    int i = 0;
+    int n ;
     scanf("%d",&n);
+    int balance;
     scanf("%d",&balance);
-    
+    int transfer_amt;
+    int count = 0;
+    int fb;
+    int sum =0;
+    int res;
+    int sum1 =0;
+    int final;
+    // int current ;
     while(i<n){
-        balance = balance+transfer;
-
-        if(balance<2000){
-            count++;
+        scanf("%d",&transfer_amt);
+        if(transfer_amt<0){
+        sum = sum + transfer_amt;
+          final = balance +transfer_amt;
+          if(final<2000){
+              count++;
+            //   current;
+          }
+        // fb = balance+sum;
+        }else{
+           sum1 = sum1+transfer_amt;
         }
         i++;
+        res = sum +sum1+balance;
     }
-    printf("Final Balance : %d",balance);
-    printf("Low Balance days: %d",count);
+    printf("final Balance : %d\n",res);
+    // printf(" Final Balance : %d",fb);
+    printf("Low balance Days = %d",count);
     return 0;
 }
