f436af1ae8f4d685c53dd823fca976ab28fe2811
manisha-mv <manishamanivannan1505@gmail.com>
2026-02-06 01:56:26 +0530

    pblm6.c

--- Array 1D/pblm6.c
+++ Array 1D/pblm6.c
@@ -2,25 +2,25 @@
 
 int main(){
     int size;
-    scanf("%d", &size);
+    scanf("%d",&size);
 
     int arr[size];
-    for(int i = 0; i < size; i++){
-        scanf("%d", &arr[i]);
+
+    for(int i =0;i<size;i++){
+        scanf("%d",&arr[i]);
     }
 
-    int j = 0;  
-
-    for(int i = 1; i < size; i++){
-        if(arr[i] != arr[j]){
-            j++;
-            arr[j] = arr[i];
+    for(int i =0;i<size;i++){
+        int d = 0;
+        for(int j = 0;j<i;j++){
+            if(arr[i]==arr[j]){
+                d =1;
+                break;
+            }
         }
+        if(d==0){
+        printf("%d",arr[i]);
     }
-
-    for(int i = 0; i <= j; i++){
-        printf("%d ", arr[i]);
     }
-
-    return 0;
+    return 0 ;
 }
