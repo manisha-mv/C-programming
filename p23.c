df00c0b53933619bb65a9d5e2099f7d85a53a85a
manisha-mv <manishamanivannan1505@gmail.com>
2026-02-07 11:41:53 +0530

    p23.c

--- p23.c
+++ p23.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
-
+#include <stdlib.h> 
+ 
 int main(){
     int size;
     scanf("%d",&size);
 
-    int arr[size],result[size];
+    int arr[size];
 
-    for(int i = 0;i<size;i++){
+    for(int i = 0 ;i<size;i++){
         scanf("%d",&arr[i]);
     }
-
-    int right= arr[size-1];
-    result[size-1]=-1;
-        // printf("%d ",right);
-
-        for(int i =size-1;i>=0;i--){
-            result[i]=right;
-            if(arr[i]>right){
-                right = arr[i];
-                // arr[i]=last;
-                // printf("%d ",arr[i]);
-                // return last;
-            }
+     int close = arr[0];
+    for(int i = 1;i<size;i++){
+        if(abs(arr[i])<abs(close)){
+            close = arr[i];
         }
-        for(int i =  0;i<size;i++){
-            printf("%d",result[i]);
+        else if(abs(arr[i]==abs(close) && arr[i]>close)){
+            close = arr[i];
         }
-        return 0 ;
+    }
+    printf("Closest = %d ",close);
+    return 0 ;
 }
+
