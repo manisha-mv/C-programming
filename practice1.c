b0a4ccf9a826f6c71ff6cc473cab41e697b308f4
manisha-mv <manishamanivannan1505@gmail.com>
2026-02-14 13:56:31 +0530

    practice strings

---
+++ practice1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    char str[20];
+    fgets(str,sizeof(str),stdin);
+
+    int count = 0;
+
+    for(int i = 0;i<str[i]!='\0';i++){
+        if(str[i]=='a' || str[i]=='e'||str[i]=='i' || str[i]=='o'||str[i]=='u' 
+        || str[i]=='A' ||str[i]=='E' || str[i]=='I'||  str[i]=='O' || str[i]=='U')
+
+        count++;
+    }
+    printf("Vowels count = %d",count);
+    return 0 ;
+}
