0c8f222538ad83f52d7d56f6927de07525b30ca7
manisha-mv <manishamanivannan1505@gmail.com>
2026-02-15 02:14:11 +0530

    p1.c

--- Strings/p1.c
+++ Strings/p1.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    char username[100];
+int main(){
+    char str[20];
+    scanf("%s",&str);
+
     int count = 0;
 
-    scanf("%s", username);   
-
-    while (username[count] != '\0') {
+    for(int i = 0;i<str[i]!='\0';i++){
         count++;
     }
-
-    printf("%d", count);
-
-    return 0;
+    printf("Length of a string : %d",count);
+     
+    return 0 ;
 }
+
+
