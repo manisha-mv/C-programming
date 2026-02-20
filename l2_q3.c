59e4223bb33c312f72f49be0f1d7b782361fa974
manisha-mv <manishamanivannan1505@gmail.com>
2026-02-20 23:51:36 +0530

    my_practice

---
+++ l2_q3.c
@@ -0,0 +1,47 @@
+// printing pattern
+// up to nth term
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10
+
+#include <stdio.h>
+
+// int main(){
+//     int n;
+//     printf("Enter the number : ");
+//     scanf("%d",&n);
+
+//     int num = 1;
+
+//     for(int i = 1;i<=n;i++){
+//         for(int j = 1;j<=i;j++){
+//             printf("%d ",num);
+//             num++;
+//         }
+//         printf("\n");
+//     }
+//     return 0 ;
+// }
+
+
+// factorial of a number
+
+
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+
+//     int fact = 1;
+
+//     for(int i = n;i>=1;i--){
+//         fact= fact*i;
+//     }
+//     printf("Fatorial of %d: %d",n,fact);
+//     return 0 ;
+// }
+
+int main(){
+    int n ;
+    
+}
